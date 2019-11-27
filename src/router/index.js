a8ee386ff6bb1cd55a47f66548d5612ce11f32e3
ruofeng <ruofeng@17weiju.com>
2019-11-27 18:29:54 +0800

    update

---
+++ src/router/index.js
@@ -0,0 +1,19 @@
+
+import vue  from 'vue';
+import  VueRouter from 'vue-router';
+
+
+//声明使用插件
+
+Vue.use(VueRouter);
+
+export default    new VueRouter({
+    routes:[
+        {
+            path:'/msite',
+            component:''
+    }
+]
+
+
+})
